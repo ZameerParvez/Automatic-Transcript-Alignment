43eb67b24d68208fb94d24dffc053cd26a65afaf
z.parvez <parvez.zameer@gmail.com>
2020-02-17 02:26:39 +0000

    Update some scripts and complete help messages and add documentation and comments to tools

--- CurrentForcedAlignementTools/aeneas/docker-aeneas.sh
+++ CurrentForcedAlignementTools/aeneas/docker-aeneas.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 HELP=\
-"No help"
+"
+This script helps with building the docker image and running the container interactively, you could alternativley use docker commands.
+
+To build and run, use:
+    ./docker-aeneas.sh -b
+    ./docker-aeneas.sh -r
+
+This will also create an aeneas folder in the current directory which is where the volume is on the host machine.
+
+-h  --help      print this help message
+-b  --build     build the docker image
+-r  --run       run the container, if a container named aeneas already exists then it will run that
+-v  --volume    can specify a volume to use for the docker image, by default it will be <current directory>/aeneas and will be mounted at /working-folder in the container (This can only be specified on the first run of the container)
+-d  --destroy   prunes all unused images and containers
+"
 
 if [ "$#" -eq 0 ]
 then
@@ -61,6 +75,6 @@ if [ $RUN = true ]; then
     if docker ps -a | grep -q "$IMAGENAME"; then
         docker container start -i $IMAGENAME
     else
-        docker container run -it -v "$VOLUME":/working-folder --name "$IMAGENAME" "$IMAGENAME"
+        docker container run -it -v "$VOLUME":/workdir --name "$IMAGENAME" "$IMAGENAME"
     fi    
 fi
